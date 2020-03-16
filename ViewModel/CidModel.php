c28b5a0ba8e3049091cf143f1dcb7430060105a7
Matias Delgado <matias@Matiass-MacBook-Pro.local>
2020-03-15 23:46:07 -0300

    fix to not use deprecated registry, remove blocks and use view model instead

---
+++ ViewModel/CidModel.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Customer Identification Document
+ *
+ * @category   Mugar
+ * @package    Mugar_CustomerIdentificationDocument
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author     Mugar (https://www.mugar.io/)
+ *
+ */
+
+namespace Mugar\CustomerIdentificationDocument\ViewModel;
+
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order;
+use Mugar\CustomerIdentificationDocument\Api\CidFieldsRepositoryInterface;
+use Magento\Framework\App\RequestInterface;
+
+class CidModel implements ArgumentInterface
+{
+
+    /**
+     * @var CidFieldsRepositoryInterface
+     */
+    protected $cidFieldsRepository;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    protected $orderRepository;
+
+    /**+
+     * @var RequestInterface
+     */
+    protected $request;
+
+    /**
+     * CidModel constructor.
+     * @param CidFieldsRepositoryInterface $cidFieldsRepository
+     * @param OrderRepositoryInterface $orderRepository
+     * @param RequestInterface $requestInterface
+     * @param array $data
+     */
+    public function __construct(
+        CidFieldsRepositoryInterface $cidFieldsRepository,
+        OrderRepositoryInterface $orderRepository,
+        RequestInterface $requestInterface,
+        array $data = []
+    ) {
+        $this->cidFieldsRepository = $cidFieldsRepository;
+        $this->orderRepository = $orderRepository;
+        $this->request = $requestInterface;
+    }
+
+    protected function getOrder(): Order
+    {
+        $id = $this->request->getParam('order_id');
+        try {
+            $order = $this->orderRepository->get($id);
+        } catch (NoSuchEntityException $e) {
+            return false;
+        } catch (InputException $e) {
+            return false;
+        }
+        return $order;
+    }
+
+    public function getCidFields()
+    {
+        if ($order = $this->getOrder()) {
+            return $this->cidFieldsRepository->getCidFields($order);
+        }
+        return false;
+    }
+}
